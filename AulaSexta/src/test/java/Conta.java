89afe3642560d238809747aca1e5ba9b213ce5e1
João <ContactorGomes@protonmail.ch>
2022-10-21 21:47:58 -0300

    ETAPA 3 - CLASSES
    Documentação dos métodos com os seus devidos parâmetros

--- AulaSexta/src/test/java/Conta.java
+++ AulaSexta/src/test/java/Conta.java
@@ -3,35 +3,59 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 
 public class Conta {
+	
+	/**
+	 * @param int1 Esse é o primeiro e único parametro do tipo Inteiro
+	 */
 	@Given("um cliente especial com saldo atual de {int} reais")
 	public void um_cliente_especial_com_saldo_atual_de_reais(Integer int1) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
+	
+	/**
+	 * @param int1 Esse é o primeiro e único parametro do tipo Inteiro
+	 */
 
 	@When("for solicitado um saque no valor de {int} reais")
 	public void for_solicitado_um_saque_no_valor_de_reais(Integer int1) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
+	
+	/**
+	 * @param int1 Esse é o primeiro e único parametro do tipo Inteiro
+	 */
 
 	@Then("deve efetuar o saque e atualizar o saldo da conta para {int} reais")
 	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer int1) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
+	
+	/**
+	 * @param int1 Esse é o primeiro e único parametro do tipo Inteiro
+	 */
 
 	@Given("um cliente comum com saldo atual de {int} reais")
 	public void um_cliente_comum_com_saldo_atual_de_reais(Integer int1) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
+	
+	/**
+	 * @param int1 Esse é o primeiro e único parametro do tipo Inteiro
+	 */
 
 	@When("solicitar um saque de {int} reais")
 	public void solicitar_um_saque_de_reais(Integer int1) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
+	
+	/**
+	 * @param int1 Esse é o primeiro e único parametro do tipo Inteiro
+	 */
 
 	@Then("não deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente")
 	public void não_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_saldo_insuficiente() {
