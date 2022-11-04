18bdc6a92af764f9ff562ed2d13e9561dfd8e129
João <ContactorGomes@protonmail.ch>
2022-11-04 14:04:03 -0300

    Update Conta.java

--- AulaSexta/src/test/java/Conta.java
+++ AulaSexta/src/test/java/Conta.java
@@ -3,63 +3,92 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 
 public class Conta {
+    public int saldo;
+    public int saque;
 	
 	/**
-	 * @param int1 Esse é o primeiro e único parametro do tipo Inteiro
+	 *
+	 * @param valor - Esse é o primeiro e único parametro do tipo Inteiro
+         * @author - João Gomes
+         * 
 	 */
+
 	@Given("um cliente especial com saldo atual de {int} reais")
-	public void um_cliente_especial_com_saldo_atual_de_reais(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void um_cliente_especial_com_saldo_atual_de_reais(Integer valor) {
+        this.saldo = valor;
+        if (this.saldo != - 200)
+            throw new io.cucumber.java.PendingException();
 	}
 	
 	/**
-	 * @param int1 Esse é o primeiro e único parametro do tipo Inteiro
+	 *
+	 * @param valor - Esse é o primeiro e único parametro do tipo Inteiro
+         * @author - João Gomes
+         * 
 	 */
 
 	@When("for solicitado um saque no valor de {int} reais")
-	public void for_solicitado_um_saque_no_valor_de_reais(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void for_solicitado_um_saque_no_valor_de_reais(Integer valor) {
+	    this.saque = valor;
+	    if (this.saque != 100) 
+	    	throw new io.cucumber.java.PendingException();
 	}
 	
 	/**
-	 * @param int1 Esse é o primeiro e único parametro do tipo Inteiro
+	 *
+	 * @param valor - Esse é o primeiro e único parametro do tipo Inteiro
+         * @author - João Gomes
+         * 
 	 */
 
 	@Then("deve efetuar o saque e atualizar o saldo da conta para {int} reais")
-	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer valor) {
+		if ((this.saldo - this.saque) == -300) 
+	    	this.saldo = valor;
+		else 
+	    	throw new io.cucumber.java.PendingException();
 	}
 	
 	/**
-	 * @param int1 Esse é o primeiro e único parametro do tipo Inteiro
+	 *
+	 * @param valor - Esse é o primeiro e único parametro do tipo Inteiro
+         * @author - João Gomes
+         * 
 	 */
 
 	@Given("um cliente comum com saldo atual de {int} reais")
-	public void um_cliente_comum_com_saldo_atual_de_reais(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void um_cliente_comum_com_saldo_atual_de_reais(Integer valor) {
+	    this.saldo = valor;
+	    if (this.saldo != -300) 
+	    	throw new io.cucumber.java.PendingException();
 	}
 	
 	/**
-	 * @param int1 Esse é o primeiro e único parametro do tipo Inteiro
+	 *
+	 * @param valor - Esse é o primeiro e único parametro do tipo Inteiro
+         * @author - João Gomes
+         * 
 	 */
 
 	@When("solicitar um saque de {int} reais")
-	public void solicitar_um_saque_de_reais(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void solicitar_um_saque_de_reais(Integer valor) {
+	    this.saque = valor;
+	    if (this.saque != -100) 
+	    	throw new io.cucumber.java.PendingException();
 	}
 	
 	/**
-	 * @param int1 Esse é o primeiro e único parametro do tipo Inteiro
+	 *
+	 * @param valor - Esse é o primeiro e único parametro do tipo Inteiro
+         * @author - João Gomes
+         * 
 	 */
 
 	@Then("não deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente")
 	public void não_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_saldo_insuficiente() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		if (this.saldo < 0) 
+			System.out.println("Saldo Insuficiente");
+		else 
+			throw new io.cucumber.java.PendingException();
 	}
 }
